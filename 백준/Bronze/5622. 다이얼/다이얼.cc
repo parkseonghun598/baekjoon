8cb72c0c6d46294c56075b128ca5055c1a384b35
parkseonghun598 <a01089277435@gmail.com>
2025-02-06 16:03:06 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.cc
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	string s;
+	cin >> s;
+	int sum = 0;
+	for (int i = 0; i < s.length(); i++) {
+		switch (s[i]) {
+		case'A':case'B':case'C':sum += 3; break;
+		case'D':case'E':case'F':sum += 4; break;
+		case'G':case'H':case'I':sum += 5; break;
+		case'J':case'K':case'L':sum += 6; break;
+		case'M':case'N':case'O':sum += 7; break;
+		case'P':case'Q':case'R':case'S':sum += 8; break;
+		case'T':case'U':case'V':sum += 9; break;
+		case'W':case'X':case'Y':case'Z':sum += 10; break;
+		}
+	}
+	cout << sum;
+}

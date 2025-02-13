0ccc87e3bfac79110c640f84f53feb255f477194
parkseonghun598 <a01089277435@gmail.com>
2025-02-13 16:02:58 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -5,22 +5,14 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int N;
-
-	int max_weight = 0, result = 0;
+	int N, max_weight = 0;
 	cin >> N;
 	vector<int> P(N);
-
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
 		cin >> P[i];
-	}
 	sort(P.begin(), P.end());
-
 	for (int i = 0; i < N; i++) {
-		if (result < P[i] * (N - i)) {
-			max_weight = P[i];
-			result = max_weight * (N - i);
-		}
+		max_weight = max(max_weight, P[i] * (N - i));
 	}
-	cout << result;
+	cout << max_weight;
 }

90a9705136f41606bc924e539ade8eeea29a1db9
parkseonghun598 <a01089277435@gmail.com>
2025-02-17 17:53:47 +0900

    [Gold V] Title: 분해합 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12348. 분해합 2/분해합 2.cc
@@ -0,0 +1,28 @@
+
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+ll f(ll x) {
+    ll sum = x;
+    while (x > 0) {
+        sum += x % 10;
+        x /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    for (ll x = n - 180; x <= n - 1; x++) {
+        if (f(x) == n) {
+            cout << x;
+            return 0;
+        }
+    }
+    cout << 0;
+}

bf63f5b6f63d7c2e10a71fad4e36a7ef841ec056
parkseonghun598 <a01089277435@gmail.com>
2025-02-19 15:57:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -6,24 +6,8 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int arr[6];
-    const int constArr[6] = { 1,1,2,2,2,8 };
-    for (int i = 0; i < 6; i++) {
-        int count = 0;
-        cin >> arr[i];
-        if (constArr[i] != arr[i]) {
-            while (constArr[i] != arr[i]) {
-                if (arr[i] < constArr[i]) {
-                    arr[i]++;
-                    count++;
-                }
-                else {
-                    arr[i]--;
-                    count--;
-                }
-            }
-        }
-        cout << count << " ";
+    int K, Q, R, B, N, P;
+    cin >> K >> Q >> R >> B >> N >> P;
+    cout << 1 - K << " " << 1 - Q << " " << 2 - R << " " << 2 - B << " " << 2 - N << " " << 8 - P;
 
-    }
 }
